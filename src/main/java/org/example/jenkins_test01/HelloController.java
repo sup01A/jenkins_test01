2c3acbe4d21e726f9f99c93e445432477c265d97
oushubo <2430504656@qq.com>
2024-10-25 01:24:27 +0800

    第一次提交

---
+++ src/main/java/org/example/jenkins_test01/HelloController.java
@@ -0,0 +1,14 @@
+package org.example.jenkins_test01;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello")
+public class HelloController {
+    @GetMapping
+    public String sayHello(){
+        return "hello Jenkins~~";
+    }
+}
---
+++ src/main/java/org/example/jenkins_test01/JenkinsTest01Application.java
@@ -0,0 +1,13 @@
+package org.example.jenkins_test01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTest01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTest01Application.class, args);
+    }
+
+}
